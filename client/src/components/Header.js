d312762e068d267ae1d63211ff360b2b3f693af6
Deena57017 <Deena57017@gmail.com>
2020-04-26 18:13:27 +0700

    classcomponent-dynamic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Header=()=>{
-    return<h1>HEADER</h1>
+class Header extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {date : new Date()};
+        setInterval(()=>this.tick(),1000)
+        console.log('constructor')
+    }
+    componentDidMount(){
+        console.log('componentDidMount')
+
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
+    tick(){
+        this.setState({date : new Date()});
+    }
+    render(){
+        return(
+        <div>{this.state.date.toLocaleTimeString()}</div>
+        )
+    }
 }
 
 export default Header;
